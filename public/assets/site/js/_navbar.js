e12b72ea23e469a2237b71b7bf063d1972f3bd4e
erickfirmo <erickluques@gmail.com>
2019-01-11 02:05:44 -0200

    correção toggle navbar

--- public/assets/site/js/_navbar.js
+++ public/assets/site/js/_navbar.js
@@ -2,16 +2,24 @@ var header = $('#header');
 var navigation = $('nav#main-navigation');
 var navigationDown = false;
 var logo = $('#logo');
+
+function changeNavbar(down) {
+    navigation.toggleClass('main-mini-navbar');
+    logo.toggleClass('logo-mini');
+    navigationDown = down;
+}
+
 $(window).scroll(function() {
-    if(window.innerWidth > 992) {
-        if($(this).scrollTop() > header.height()-60 && $(this).scrollTop() < header.height()+20 && navigationDown == false) {
-            navigationDown = true;
-            navigation.toggleClass('main-mini-navbar');
-            logo.toggleClass('logo-mini');
-        } else if($(this).scrollTop() > header.height()-60 && $(this).scrollTop() < header.height()+20 && navigationDown == true){
-            navigationDown == false;
-            navigation.toggleClass('main-mini-navbar');
-            logo.toggleClass('logo-mini');
+    if($(this).scrollTop() > header.height()-60 && $(this).scrollTop() < header.height()+20) {
+        if(navigationDown == true) {
+            changeNavbar(true);
+        } else if(navigationDown == false) {
+            changeNavbar(false);
         }
     }
 })
+
+$(document).ready(function() {
+    changeNavbar(true);
+});
+
