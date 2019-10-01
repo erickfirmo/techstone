ae92f7e86d438457d142856e09dc32d8d7cc1a97
Erick <erickluques@gmail.com>
2019-09-30 23:28:32 -0300

    Update mensagens/_add-favorita.js

--- public/assets/admin/js/mensagens/_add-favorita.js
+++ public/assets/admin/js/mensagens/_add-favorita.js
@@ -1,9 +1,4 @@
-var mensagem_id = null;
-var _token = null;
 var favorite_icon = null;
-
-//$('.mailbox-star').click( function(event){event.stopPropagation()});
-
 $('.add-favorita').on('click', function() {
     mensagem_id = $(this).find('input[name="mensagem_id"]').val();
     _token = $(this).find('input[name="_token"]').val();
@@ -15,31 +10,37 @@ $('.add-favorita').on('click', function() {
             data: {_token:_token,mensagem_id:mensagem_id},
             success: function(data) {
                 if($.isEmptyObject(data.error)) {
+                    msg_favorita = JSON.parse(data);
+                    msg_favorita = $('.msg-'+msg_favorita);
+                    if(msg_favorita.hasClass('is_favorite') && location.hash == '#favoritas') {
+                        msg_favorita.css('display', 'none');
+                    } else {
+                        msg_favorita.removeClass('is_trashed');
+                        msg_favorita.css('display', 'show');
+                    }
                     if(favorite_icon.hasClass('fa-star') && !favorite_icon.hasClass('fa-star-o')) {
                         favorite_icon.removeClass('fa-star-o');
                         favorite_icon.addClass('fa-star');
-                        //alert('A mensagem foi adicionada aos favoritos.');
+                        msg_favorita.addClass('is_favorite');
                         swal('A mensagem foi adicionada aos favoritos !', " ", "success", {
 							buttons: false,
-							type: "success",
      						timer: 1200
-						});
+                        });
                     } else if(favorite_icon.hasClass('fa-star-o') && !favorite_icon.hasClass('fa-star')) {
                         favorite_icon.removeClass('fa-star');
                         favorite_icon.addClass('fa-star-o');
-                        //alert('A mensagem foi removida dos favoritos !');
+                        msg_favorita.removeClass('is_favorite');
                         swal('A mensagem foi removida dos favoritos !', " ", "success", {
 							buttons: false,
-							type: "success",
      						timer: 1200
-						});
+                        });
                     }
                 } else {
                     swal("Ops, algo deu errado !", {
                         icon: "warning",
                         timer: 1200,
                         buttons: false,
-                      }); 
+                    }); 
                 }
             }
         });
@@ -47,6 +48,6 @@ $('.add-favorita').on('click', function() {
         swal("Selecione uma mensagem !", {
             timer: 1200,
             buttons: false,
-          });
+        });
     }
 });
